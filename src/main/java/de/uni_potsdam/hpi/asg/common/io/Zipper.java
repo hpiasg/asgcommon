87043bfc0e8ef7b89d30fb77274adcd7d32ea5f6
Norman Kluge <norman.kluge@hpi.de>
2016-07-28 17:06:18 +0200

    add more costumisable interface to zipper

--- src/main/java/de/uni_potsdam/hpi/asg/common/io/Zipper.java
+++ src/main/java/de/uni_potsdam/hpi/asg/common/io/Zipper.java
@@ -32,71 +32,77 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Zipper {
-	public final static Logger logger = LogManager.getLogger();
-	
-	private static Zipper instance;
-	
-	private String workingdir;
-	
-	private Zipper() {};
-	
-	public static Zipper getInstance() {
-		if(instance == null) {
-			instance = new Zipper();
-		}
-		return instance;
-	}
-	public void setWorkingdir(String workingdir) {
-		this.workingdir = workingdir;
-	}
-	
-	public boolean zip(File zipFile) {
-		byte[] buffer = new byte[1024];
-		try {
-			List<File> filelist = new ArrayList<File>();
-			if(!getFileList(new File(workingdir), filelist)) {
-				logger.error("Could not get list of files for zipping");
-				return false;
-			}
-			
-			FileOutputStream fos = new FileOutputStream(zipFile);
-			ZipOutputStream zos = new ZipOutputStream(fos);
-			FileInputStream in = null;
-			for(File file : filelist) {
-				String subname = file.getAbsolutePath().substring(workingdir.length(), file.getAbsolutePath().length());
-				ZipEntry ze = new ZipEntry(subname);
-				zos.putNextEntry(ze);
-				in = new FileInputStream(file);
-				int len;
-				while ((len = in.read(buffer)) > 0) {
-					zos.write(buffer, 0, len);
-				}
-				in.close();
-				zos.closeEntry();
-			}
-			zos.close();
-			return true;
-		} catch (IOException e) {
-			logger.error(e.getLocalizedMessage());
-			return false;
-		}
-	}
+    public final static Logger logger = LogManager.getLogger();
 
-	private boolean getFileList(File file, List<File> files) {
-		if(file == null) {
-			return false;
-		}
-		if(file.isFile()) {
-			files.add(file);
-		} else if(file.isDirectory()) {
-			for(String str : file.list()) {
-				if(!getFileList(new File(file, str), files)) {
-					logger.error("Could not zip subentry: " + str);
-				}
-			}
-		} else {
-			logger.warn("Unknown filesystem entry: " + file.toString());
-		}
-		return true;
-	}
+    private static Zipper      instance;
+
+    private String             workingdir;
+
+    private Zipper() {
+    };
+
+    public static Zipper getInstance() {
+        if(instance == null) {
+            instance = new Zipper();
+        }
+        return instance;
+    }
+
+    public void setWorkingdir(String workingdir) {
+        this.workingdir = workingdir;
+    }
+
+    public boolean zip(File zipFile) {
+        return zip(zipFile, new File(workingdir));
+    }
+
+    public boolean zip(File zipFile, File dir) {
+        byte[] buffer = new byte[1024];
+        try {
+            List<File> filelist = new ArrayList<File>();
+            if(!getFileList(dir, filelist)) {
+                logger.error("Could not get list of files for zipping");
+                return false;
+            }
+
+            FileOutputStream fos = new FileOutputStream(zipFile);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+            FileInputStream in = null;
+            for(File file : filelist) {
+                String subname = file.getAbsolutePath().substring(dir.getAbsolutePath().length(), file.getAbsolutePath().length());
+                ZipEntry ze = new ZipEntry(subname);
+                zos.putNextEntry(ze);
+                in = new FileInputStream(file);
+                int len;
+                while((len = in.read(buffer)) > 0) {
+                    zos.write(buffer, 0, len);
+                }
+                in.close();
+                zos.closeEntry();
+            }
+            zos.close();
+            return true;
+        } catch(IOException e) {
+            logger.error(e.getLocalizedMessage());
+            return false;
+        }
+    }
+
+    private boolean getFileList(File file, List<File> files) {
+        if(file == null) {
+            return false;
+        }
+        if(file.isFile()) {
+            files.add(file);
+        } else if(file.isDirectory()) {
+            for(String str : file.list()) {
+                if(!getFileList(new File(file, str), files)) {
+                    logger.error("Could not zip subentry: " + str);
+                }
+            }
+        } else {
+            logger.warn("Unknown filesystem entry: " + file.toString());
+        }
+        return true;
+    }
 }

f7c8182afdb7c87b95ce9e71ffa06d2750e91b53
Norman Kluge <norman.kluge@hpi.de>
2016-07-28 17:21:16 +0200

    add unzip

--- src/main/java/de/uni_potsdam/hpi/asg/common/io/Zipper.java
+++ src/main/java/de/uni_potsdam/hpi/asg/common/io/Zipper.java
@@ -21,11 +21,13 @@ package de.uni_potsdam.hpi.asg.common.io;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 import org.apache.logging.log4j.LogManager;
@@ -56,11 +58,11 @@ public class Zipper {
         return zip(zipFile, new File(workingdir));
     }
 
-    public boolean zip(File zipFile, File dir) {
+    public boolean zip(File zipFile, File srcDir) {
         byte[] buffer = new byte[1024];
         try {
             List<File> filelist = new ArrayList<File>();
-            if(!getFileList(dir, filelist)) {
+            if(!getFileList(srcDir, filelist)) {
                 logger.error("Could not get list of files for zipping");
                 return false;
             }
@@ -69,7 +71,7 @@ public class Zipper {
             ZipOutputStream zos = new ZipOutputStream(fos);
             FileInputStream in = null;
             for(File file : filelist) {
-                String subname = file.getAbsolutePath().substring(dir.getAbsolutePath().length(), file.getAbsolutePath().length());
+                String subname = file.getAbsolutePath().substring(srcDir.getAbsolutePath().length(), file.getAbsolutePath().length());
                 ZipEntry ze = new ZipEntry(subname);
                 zos.putNextEntry(ze);
                 in = new FileInputStream(file);
@@ -102,6 +104,39 @@ public class Zipper {
             }
         } else {
             logger.warn("Unknown filesystem entry: " + file.toString());
+        }
+        return true;
+    }
+
+    public boolean unzip(File zipFile, File trgDir) {
+        try {
+            byte[] buffer = new byte[1024];
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+            ZipEntry ze = null;
+            while((ze = zis.getNextEntry()) != null) {
+                String filename = ze.getName();
+                File newFile = new File(trgDir, filename);
+
+                File parent = new File(newFile.getParent());
+                parent.mkdirs();
+
+                FileOutputStream fos = new FileOutputStream(newFile);
+
+                int len;
+                while((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+
+                fos.close();
+            }
+            zis.closeEntry();
+            zis.close();
+        } catch(FileNotFoundException e) {
+            logger.error(e.getLocalizedMessage());
+            return false;
+        } catch(IOException e) {
+            logger.error(e.getLocalizedMessage());
+            return false;
         }
         return true;
     }

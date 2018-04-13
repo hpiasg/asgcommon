c436d026cd393935189c2ca03bed1cebdc066529
Norman Kluge <norman.kluge@hpi.de>
2018-04-13 19:41:41 +0200

    add constants for protocol

--- src/main/java/de/uni_potsdam/hpi/asg/common/misc/CommonConstants.java
+++ src/main/java/de/uni_potsdam/hpi/asg/common/misc/CommonConstants.java
@@ -30,37 +30,42 @@ public class CommonConstants {
     }
 
     // env abstract directories
-    public static final String USERDIR_STR                = "$USER-DIR";
-    public static final String BASEDIR_STR                = "$BASEDIR";
-    public static final String BASEDIR_REGEX              = "\\" + BASEDIR_STR;
+    public static final String USERDIR_STR                  = "$USER-DIR";
+    public static final String BASEDIR_STR                  = "$BASEDIR";
+    public static final String BASEDIR_REGEX                = "\\" + BASEDIR_STR;
 
     // default directories
-    public static final String DEF_TECH_DIR_STR           = "$BASEDIR/tech";
-    public static final File   DEF_TECH_DIR_FILE          = BasedirHelper.replaceBasedirAsFile(DEF_TECH_DIR_STR);
-    public static final File   DEF_BALSA_TECH_DIR_FILE    = BasedirHelper.replaceBasedirAsFile("$BASEDIR/tools/balsa/share/tech");
-    public static final String DEF_CONFIG_DIR_STR         = "$BASEDIR/config";
-    public static final File   DEF_CONFIG_DIR_FILE        = BasedirHelper.replaceBasedirAsFile(DEF_CONFIG_DIR_STR);
-    public static final File   DEF_BIN_DIR_FILE           = BasedirHelper.replaceBasedirAsFile("$BASEDIR/bin");
-    public static final File   DEF_TEMPLATE_DIR_FILE      = BasedirHelper.replaceBasedirAsFile("$BASEDIR/templates");
+    public static final String DEF_TECH_DIR_STR             = "$BASEDIR/tech";
+    public static final File   DEF_TECH_DIR_FILE            = BasedirHelper.replaceBasedirAsFile(DEF_TECH_DIR_STR);
+    public static final File   DEF_BALSA_TECH_DIR_FILE      = BasedirHelper.replaceBasedirAsFile("$BASEDIR/tools/balsa/share/tech");
+    public static final String DEF_CONFIG_DIR_STR           = "$BASEDIR/config";
+    public static final File   DEF_CONFIG_DIR_FILE          = BasedirHelper.replaceBasedirAsFile(DEF_CONFIG_DIR_STR);
+    public static final File   DEF_BIN_DIR_FILE             = BasedirHelper.replaceBasedirAsFile("$BASEDIR/bin");
+    public static final File   DEF_TEMPLATE_DIR_FILE        = BasedirHelper.replaceBasedirAsFile("$BASEDIR/templates");
+    public static final String DEF_PROTOCOL_DIR_STR         = "$BASEDIR/protocols";
+    public static final File   DEF_PROTOCOL_DIR_FILE        = BasedirHelper.replaceBasedirAsFile(DEF_PROTOCOL_DIR_STR);
 
     // script files
-    public static final String SCRIPT_FILE_EXTENSION      = OperatingSystemHelper.getScriptExtension();
+    public static final String SCRIPT_FILE_EXTENSION        = OperatingSystemHelper.getScriptExtension();
 
     // output files
-    public static final String ZIP_FILE_EXTENSION         = ".zip";
-    public static final String LOG_FILE_EXTENSION         = ".log";
+    public static final String ZIP_FILE_EXTENSION           = ".zip";
+    public static final String LOG_FILE_EXTENSION           = ".log";
 
     // technology files
-    public static final String EXPORT_TECH_FILE_EXTENSION = ".tech";
-    public static final String XMLTECH_FILE_EXTENSION     = ".xml";
-    public static final String GENLIB_FILE_EXTENSION      = "_gen.lib";
-    public static final String LIBERTY_FILE_EXTENSION     = "_liberty.lib";
-    public static final String ADDINFO_FILE_EXTENSION     = "_addInfo.json";
+    public static final String EXPORT_TECH_FILE_EXTENSION   = ".tech";
+    public static final String XMLTECH_FILE_EXTENSION       = ".xml";
+    public static final String GENLIB_FILE_EXTENSION        = "_gen.lib";
+    public static final String LIBERTY_FILE_EXTENSION       = "_liberty.lib";
+    public static final String ADDINFO_FILE_EXTENSION       = "_addInfo.json";
+
+    // protocol files
+    public static final String PROTOCOL_MAIN_FILE_EXTENSION = ".xml";
 
     // misc files
-    public static final String BALSA_FILE_EXTENSION       = ".balsa";
-    public static final String BREEZE_FILE_EXTENSION      = ".breeze";
-    public static final String VERILOG_FILE_EXTENSION     = ".v";
-    public static final String STG_FILE_EXTENSION         = ".g";
+    public static final String BALSA_FILE_EXTENSION         = ".balsa";
+    public static final String BREEZE_FILE_EXTENSION        = ".breeze";
+    public static final String VERILOG_FILE_EXTENSION       = ".v";
+    public static final String STG_FILE_EXTENSION           = ".g";
 
 }
